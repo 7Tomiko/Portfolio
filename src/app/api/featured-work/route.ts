b56b92136ae56d21bab32d35bbb97277b3f17087
Paul Caumont <paul.caumont@stjosup.com>
2026-03-23 16:00:00 +0100

    Correction finale ??

--- src/app/api/featured-work/route.ts
+++ src/app/api/featured-work/route.ts
@@ -5,13 +5,13 @@ const featureWork = [
         title: "La réalité augmentée dans la médecine",
         description: "Exploration de l'application de la réalité augmentée dans le domaine médical.",
         roles: ["UX Designer", "Framer Designer"],
-        image: "/images/feature-work/feature-img-1.png"
+        image: "/portfolio/images/feature-work/feature-img-1.png"
     },
     {
         title: "Conception d'une identité visuelle pour un fournisseur de soins de santé innovant",
         description: "Création d'une identité visuelle distinctive et d'un langage de conception pour construire la confiance et l'empathie chez un prestataire de soins de santé à l'esprit innovant.",
         roles: ["UX Designer", "Framer Designer"],
-        image: "/images/feature-work/feature-img-2.png"
+        image: "/portfolio/images/feature-work/feature-img-2.png"
     }
 ]
 
